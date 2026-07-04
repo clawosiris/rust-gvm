5af038b78f3a19fa33949f7014bc63535e06af3e
Robert Pelfrey <robert.pelfrey@greenbone.net>
2026-07-04 05:01:21 +0200

    feat(gmp): add singular feed helper (#302)
    Co-authored-by: Robert Pelfrey <184965853+robpelf@users.noreply.github.com>

--- crates/gvm-gmp/src/commands/feed.rs
+++ crates/gvm-gmp/src/commands/feed.rs
@@ -5,19 +5,37 @@
 
 use gvm_protocol::XmlCommand;
 
+use crate::enums::FeedType;
+
 /// Build a `get_feeds` request.
 #[must_use]
 pub fn get_feeds() -> XmlCommand {
     XmlCommand::new("get_feeds")
 }
 
+/// Build a `get_feed` request.
+#[must_use]
+pub fn get_feed(feed_type: FeedType) -> XmlCommand {
+    XmlCommand::new("get_feeds").attribute("type", feed_type.as_gmp_str())
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::commands::feed::get_feeds;
+    use crate::commands::feed::{get_feed, get_feeds};
     use crate::common::xml;
+    use crate::FeedType;
 
     #[test]
     fn get_feeds_builds_xml() {
         assert_eq!(xml(get_feeds()), "<get_feeds/>");
+    }
+
+    #[test]
+    fn get_feed_builds_xml() {
+        assert_eq!(xml(get_feed(FeedType::Nvt)), "<get_feeds type=\"NVT\"/>");
+        assert_eq!(
+            xml(get_feed(FeedType::Gvmd)),
+            "<get_feeds type=\"GVMD_DATA\"/>"
+        );
     }
 }
